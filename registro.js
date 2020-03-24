e80d6c84cf75fdc4ad0c13ae12792aac91324fef
jorgedly <jorgekl.1994@gmail.com>
2020-03-24 11:40:05 -0600

    Agregadas pruebas para validar contrasenas, correos y registro correcto de nuevos usuarios. 201314761

---
+++ registro.js
@@ -0,0 +1,35 @@
+function registrar(nombre, correo, contrasena, fechaNacimiento) {
+    if (validarContrasena(contrasena) && validarCorreo(correo)) {
+        const usuario = { nombre, correo, contrasena, fechaNacimiento };
+        const usuarios = JSON.parse(localStorage.getItem('usuarios'));
+        usuarios.push(usuario);
+        localStorage.setItem('usuarios', JSON.stringify(usuarios));
+        return true;
+    }
+    return false;
+}
+
+function validarContrasena(contrasena) {
+    const patron = /[A-Za-z0-9]+/;
+    if (contrasena.length >= 8 && patron.test(contrasena)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validarCorreo(correo) {
+    const usuarios = JSON.parse(localStorage.getItem('usuarios'));
+    if (!correo.includes('@')) {
+        return false;
+    }
+    for (let i = 0; i < usuarios.length; i++) {
+        const usuario = usuarios[i];
+        if (usuario.correo.localeCompare(correo) === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+module.exports = { registrar, validarContrasena, validarCorreo };
---
+++ registro.test.js
@@ -0,0 +1,43 @@
+const { registrar, validarContrasena, validarCorreo } = require('./registro');
+
+describe('validarContrasena', () => {
+    test('contrasena corta debe ser incorrecta', () => {
+        expect(validarContrasena('123')).toBe(false);
+    });
+    test('contrasena con caracteres invalidos debe ser incorrecta', () => {
+        expect(validarContrasena('12!3')).toBe(false);
+    });
+    test('contrasena correcta', () => {
+        expect(validarContrasena('aBc123cd')).toBe(true);
+    });
+});
+
+describe('validarCorreo', () => {
+    test('correo no cumple con formato', () => {
+        expect(validarCorreo('correo1')).toBe(false);
+    });
+    test('correo ya existe', () => {
+        const usuario = {
+            correo: 'correo1@gmail.com',
+            nombre: 'nombre1',
+            contrasena: 'contrasena1',
+            fechaNacimiento: '03-06-1994'
+        };
+        const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+        usuarios.push(usuario);
+        localStorage.setItem('usuarios', JSON.stringify(usuarios));
+        expect(validarCorreo('correo1@gmail.com')).toBe(false);
+    });
+    test('correo correcto', () => {
+        expect(validarContrasena('correo2@gmail.com')).toBe(true);
+    });
+});
+
+describe('registrar', () => {
+    test('datos de usuario invalidos', () => {
+        expect(registrar('jorge', 'correo1@gmail.com', '123', '03-06-1994')).toBe(false);
+    });
+    test('datos correctos', () => {
+        expect(registrar('jorge', 'correo2@gmail.com', '123abc45', '03-06-1994')).toBe(true);
+    });
+});
